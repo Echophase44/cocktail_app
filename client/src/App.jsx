b1b1609f806946f07272bdfcb3d89853a39e8403
Nick <Echophase44@github.com>
2023-04-30 15:54:27 -0400

    Added the beginnings to the API fetch

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,9 +5,25 @@ import Home from './Pages/Home'
 
 
 function App() {
+  const [searchParam, setSearchParam] = useState("")
   const [searchResults, setSearchResults] = useState({})
 
-  
+  useEffect(() => {
+    if(searchParam === ""){
+      console.log("Effect not searched")
+      return
+    }
+
+    let drinkType = searchParam.trim().replace(" ", "_")
+    fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${drinkType}`)
+      .then((res) => res.json())
+      .then((data)=> {
+        console.log(data)
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+  }, [searchParam])
 
   return (
     <>

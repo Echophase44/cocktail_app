4d65061ca929cbfb589efac00ab70a63f0ccc5fd
Nick <Echophase44@github.com>
2023-05-02 16:14:01 -0400

    WIP - images now display in the center display when searched.

--- client/src/components/Display.jsx
+++ client/src/components/Display.jsx
@@ -5,12 +5,23 @@ import '../styles/display.css'
 function Display(props) {
   const { searchResults } = props
 
+    const drinkImages = searchResults.drinks?.map((element, index) => (
+      <div key={index} className='display_cardContainer'>
+        <img className="display_cardDrinkImage" src={element.strDrinkThumb} alt="" />
+        <p>This is quite the image!</p>
+      </div>
+    ))
+
   return (
     <section className='display_main'>
-    {Object.keys(searchResults).length === 0 && <div className='display_empty'>
+    {/* {Object.keys(searchResults).length === 0 && <div className='display_empty'>
       <img src={shaker} alt="Drink shaker" />
       <p>Search for a drink to see results.</p>
-    </div> }
+    </div> } */}
+    <div className='display_allCards'>
+      {drinkImages}
+    </div>
+    
     </section>
   )
 }

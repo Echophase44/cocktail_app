4e39aa91fab1f04f190e4236dcb00d25954f2bfa
Nick <Echophase44@github.com>
2023-05-02 21:44:19 -0400

    Cleaned up the image display search results and added a display for having no results

--- client/src/components/Display.jsx
+++ client/src/components/Display.jsx
@@ -8,19 +8,28 @@ function Display(props) {
     const drinkImages = searchResults.drinks?.map((element, index) => (
       <div key={index} className='display_cardContainer'>
         <img className="display_cardDrinkImage" src={element.strDrinkThumb} alt="" />
-        <p>This is quite the image!</p>
+        <p className='display_cardQuote'>This is quite the image!</p>
       </div>
     ))
 
+    console.log(searchResults)
+
   return (
     <section className='display_main'>
-    {/* {Object.keys(searchResults).length === 0 && <div className='display_empty'>
-      <img src={shaker} alt="Drink shaker" />
-      <p>Search for a drink to see results.</p>
-    </div> } */}
-    <div className='display_allCards'>
-      {drinkImages}
-    </div>
+
+      {Object.keys(searchResults).length === 0 && <div className='display_empty'>
+        <img src={shaker} alt="Drink shaker" />
+        <p>Search for a drink to see results.</p>
+      </div> }
+
+      {searchResults.drinks?.length >= 1 && <div className='display_allCards'>
+        {drinkImages}
+      </div>}
+
+      {searchResults.drinks === null && <div className='display_empty'> 
+      <p>No results found!</p>
+      <p>Try searching for something else!</p>
+      </div>}
     
     </section>
   )

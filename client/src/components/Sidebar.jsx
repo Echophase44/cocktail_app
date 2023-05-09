d6656841d4b8effbc532a4f9e73281c9231a5b55
Nick <Echophase44@github.com>
2023-05-09 06:45:21 -0400

    Updated a couple stylings. Instructions section now opens/closes with data.

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -28,25 +28,29 @@ function Sidebar(props) {
   }
 
   function toggleInstructionsView() {
-    
+    setPanels(prevPanels => ({
+      ...prevPanels,
+      instructionsPanel: !prevPanels.instructionsPanel
+    }))
   }
 
   return(
     <section className="sidebar_container">
       <h2>Offical Cocktails</h2>
       <p>Search through thousands of premium cocktails.</p>
-      <button className={"sidebar_button" + (panels.ingredientsPanel ? " open" : "")} onClick={toggleIngredientsView}>Ingredients</button>
+      <button className={"sidebar_button" + (panels.ingredientsPanel ? " open" : "")} onClick={toggleIngredientsView}><strong>Ingredients</strong></button>
         <div className="sidebar_ingredientsContainer">
           <ul className={"sidebar_ingredientsWrapper" + (panels.ingredientsPanel ? " open" : "")}>
-            {ingredients.length === 0 && <li>No Ingredients to display.</li>}
+            {ingredients.length === 0 && <li>No drink selected.</li>}
             {currentIngredients}
           </ul>
           <ul className={"sidebar_measurementsWrapper" + (panels.ingredientsPanel ? " open" : "")}>
             {currentMeasurements}
           </ul>
         </div>
-      <button className="sidebar_button">Instructions</button>
-        <div className="sidebar_instructionsContainer">
+      <button className={"sidebar_button" + (panels.instructionsPanel ? " open" : "")} onClick={toggleInstructionsView}><strong>Instructions</strong></button>
+        <div className={"sidebar_instructionsContainer" + (panels.instructionsPanel ? " open" : "")}>
+          {instructions.length === 0 && <span>No drink selected.</span>}
           {currentInstructions}
         </div>
       <p className="sidebar_note">All listed drinks are supplied by the CocktailDB API.</p>
